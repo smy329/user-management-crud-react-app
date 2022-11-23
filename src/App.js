fbe546ba4ea96a553aaead2f29557cec31250708
Syed Mohammad Yasir <smy329@gmail.com>
2022-11-23 14:43:05 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,159 @@
 import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import UserForm from './Components/UserForm';
+
+const URL = "https://rest-api-without-db.herokuapp.com/users"
 
 function App() {
+    const [users, setUsers] = useState(null)
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+    //upadate
+    const [selectedUser, setSelectedUser] = useState({
+        username: '',
+        email: ''
+    })
+    const [updateFlag, setUpdateFlag] = useState(false);
+    const [selectedUserId, setSetSelectedUserId] = useState('');
+
+    const getAllUsers = () => {
+        fetch(URL)
+          .then((res) => {
+            if (!res.ok) {
+              throw Error("Could not fetch data");
+            }
+            return res.json();
+          })
+          .then((data) => {
+            console.log(data.users);
+            setUsers(data.users);
+          })
+          .catch((err) => {
+            setError(err.message);
+          })
+          .finally(() => {
+            setIsLoading(false);
+          });
+    }
+
+    useEffect(() => {
+        getAllUsers()
+    }, [])
+
+    //delete user
+    const handleDelete = (id) => {
+        fetch(URL + `/${id}`, {
+            method: 'DELETE'
+        })
+          .then((res) => {
+            if (!res.ok) {
+              throw Error("Could not delete data");
+            }
+            getAllUsers()
+          })
+          .catch((err) => {
+            setError(err.message);
+          })
+    }
+
+    const addUser = (user) => {
+        fetch(URL, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(user)
+        })
+          .then((res) => {
+            if (res.status === 201) {
+              getAllUsers()
+            } else {
+                throw new Error("Failed to create new user")
+            }
+          })
+          .catch((err) => {
+            setError(err.message);
+          });
+    }
+
+    const handleEdit = (id) => {
+        setSetSelectedUserId(id)
+        setUpdateFlag(true)
+        const filteredData = users.filter((user) => user.id === id)
+        setSelectedUser({
+            username: filteredData[0].username,
+            email: filteredData[0].email
+        })
+    }
+
+    const handleUpdate = (user) => {
+        fetch(URL + `/${selectedUserId}`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(user),
+        })
+          .then((res) => {
+            if (!res.ok) {
+              throw Error("Could not update data");
+            }
+            getAllUsers();
+            setUpdateFlag(false)
+          })
+          .catch((err) => {
+            setError(err.message);
+          });
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> User Management App </h1>
+      {isLoading && <h2>Loading...</h2>}
+      {error && <h2>{error}</h2>}
+
+      {updateFlag ? (
+        <UserForm
+          btnText="Edit User"
+          selectedUser={selectedUser}
+          handleSubmitData={handleUpdate}
+        />
+      ) : (
+                  <UserForm
+                      btnText="Add User"
+                      handleSubmitData={addUser}
+                  />
+      )}
+
+      <section>
+        {users &&
+          users.map((user) => {
+            return (
+              <article className="card" key={user.id}>
+                <p>{user.username}</p>
+                <p>{user.email}</p>
+                <button
+                  className="btn"
+                  onClick={() => {
+                    handleEdit(user.id);
+                  }}
+                >
+                  Edit
+                </button>
+                <button
+                  className="btn"
+                  onClick={() => {
+                    handleDelete(user.id);
+                  }}
+                >
+                  Delete
+                </button>
+              </article>
+            );
+          })}
+      </section>
     </div>
   );
 }
---
+++ src/Components/UserForm.js
@@ -0,0 +1,72 @@
+import React, {useEffect} from 'react'
+import PropTypes from 'prop-types'
+import { useState } from 'react'
+
+const UserForm = ({ handleSubmitData, btnText, selectedUser }) => {
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+  });
+    
+    useEffect(() => {
+        setUser({
+            username: selectedUser.username,
+            email: selectedUser.email
+      })
+    }, [selectedUser])
+    
+
+  const handleChange = (e) => {
+    const selectedField = e.target.name;
+    const selectedValue = e.target.value;
+    setUser((prevState) => {
+      return { ...prevState, [selectedField]: selectedValue };
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+      handleSubmitData(user);
+      setUser({
+          username: '',
+          email: ''
+      })
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="field-group">
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          value={user.username}
+          onChange={handleChange}
+          required
+        />{" "}
+        <br />
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          id="email"
+          name="email"
+          value={user.email}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <button className="btn" type="submit">
+        {btnText}
+      </button>
+    </form>
+  );
+};
+
+UserForm.defaultProps = {
+    selectedUser: {
+        username: '',
+        email: ''
+    }
+}
+
+export default UserForm
